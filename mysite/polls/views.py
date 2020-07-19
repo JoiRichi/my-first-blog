e430908b4da392338264ac2d1213896305e49444
Richard <lordrichado@gmail.com>
2020-07-19 12:55:51 -0700

    Changed the HTML for the site.

---
+++ mysite/polls/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,"home.html")
+
+def another(request):
+    return render(request, 'nextpage.html')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
